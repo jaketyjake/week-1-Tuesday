6abef80996d9305a164bf79b0da7ef53580d9db9
jaketyjake <jaketyjake@gmail.com>
2018-05-01 12:21:25 -0500

    removed erroneous

--- tipcalc/tipcalc.py
+++ tipcalc/tipcalc.py
@@ -2,9 +2,7 @@
 total = float(input("Enter the total:"))
 tip_percent = float(input("Enter percentage of tip to add:"))
 tip_amount = float(tip_percent / 100)
-#float(tip_amount)
 total = (tip_amount * total) + total
-#round(total, 2)
 print("%.2f" % total)
 # two decmial places --> %.2f
 
